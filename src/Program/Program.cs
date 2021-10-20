af9c78950b61e1caaf1d43ea60d2da794b51d933
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2021-10-20 17:02:51 -0300

    Parte 1,2 y 3 funcionando

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -8,6 +9,22 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+        PictureProvider provider = new PictureProvider();
+        IPicture picture = provider.GetPicture(@"beer.jpg");
+
+        IPipe nulo = new PipeNull();
+        IFilter negative = new FilterNegative();
+        IPipe serialPipe1 = new PipeSerial(negative,nulo);
+        IFilter greyscale = new FilterGreyscale();
+        IPipe serialPipe2 = new PipeSerial(greyscale, serialPipe1);// termina de aplicar los filtros 
+         
+        IPicture picture1 = serialPipe2.Send(picture);// guarda los cambios de los filtros 
+        
+        provider.SavePicture(picture1 , @"cerveija.jpg");
+
+        TwitterImage twitter = new TwitterImage();
+        Console.WriteLine(twitter.PublishToTwitter("MESSIENTO TRISTE", @"cerveija.jpg" ));
+
         }
     }
 }
